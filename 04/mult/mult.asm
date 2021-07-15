1bff7db2f000971d36eb7d7cc6a119ae3bfb7b1c
Tiago Ceccon <ceccon.t@hotmail.com>
2021-07-14 23:48:39 -0300

    Project 04 - Implement multiplication in Hack

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,65 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// Algorithm:
+// Let 'n' be the number on R0 and 'base' be the number on R1
+// Initialize 'sum' to 0
+// And do n times sum = sum + base 
+
+// total_iters = R0 
+    @R0
+    D=M 
+    @total_iters 
+    M=D 
+// base = R1 
+    @R1 
+    D=M 
+    @base 
+    M=D 
+// sum = 0
+    @sum 
+    M=0
+// i = 0
+    @i
+    M=0
+// initialize register for result (R2) with 0
+    @R2 
+    M=0
+
+(LOOP)
+    @i 
+    D=M 
+    
+    // while i != total_iters:
+    @total_iters 
+    D=M-D
+    @RESULT
+    D;JEQ
+
+    // sum = sum + base 
+    @base
+    D=M 
+    @sum 
+    M=M+D 
+
+    // i += 1
+    @i 
+    M=M+1
+
+    // go to next iteration 
+    @LOOP 
+    0;JMP
+
+// R2 = sum
+(RESULT)
+    @sum
+    D=M 
+    @R2 
+    M=D
+
+// infinite loop to finalize program
+(END)
+    @END
+    0;JMP
+
---
+++ 04/mult/sketchmult.py
@@ -0,0 +1,16 @@
+# High level code in Python to sketch implementation
+
+r0 = 3
+r1 = 5
+
+# start alg
+sum = 0
+numiters = r0 
+base = r1
+i = 0
+
+while i != numiters:
+    sum = sum + base 
+    i += 1
+
+print(sum, numiters, i)
