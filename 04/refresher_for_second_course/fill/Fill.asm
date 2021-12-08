32dfe62aac20127fbea474936d901788acd1d608
Tiago Ceccon <ceccon.t@hotmail.com>
2021-12-08 16:13:44 -0300

    Refresher for second course - Reimplement fill on project 04

--- 04/refresher_for_second_course/fill/Fill.asm
+++ 04/refresher_for_second_course/fill/Fill.asm
@@ -12,3 +12,88 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Initialization
+    @SCREEN
+    D=A 
+
+    @first
+    M=D
+
+    @current
+    M=D
+
+    @KBD 
+    D=A 
+
+    @last
+    M=D
+
+// Check if key is pressed 
+(CHECKINPUT)
+    // Reinitialize current position
+    @SCREEN
+    D=A 
+    @current 
+    M=D
+
+    // Read keyboard
+    @KBD
+    D=M 
+
+    // If any key pressed, fill with black 
+    @LOOPFILLBLACK
+    D;JNE
+
+    // Otherwise, fill with white
+
+(LOOPFILLWHITE)
+    @current
+    D=M 
+
+    // Fill position on screen
+    A=D
+    M=0
+
+    // Increment position on screen 
+    @current
+    M=M+1
+    D=M
+
+    // Check if ended
+    @last 
+    D=M-D
+    @LOOPFILLWHITE
+    D;JNE
+
+    @CHECKINPUT
+    0;JMP
+
+//  DIVISION BETWEEN LOOPS
+
+(LOOPFILLBLACK)
+    @current
+    D=M 
+
+    // Fill position on screen
+    A=D
+    M=-1
+
+    // Increment position on screen 
+    @current
+    M=M+1
+    D=M
+
+    // Check if ended
+    @last 
+    D=M-D
+    @LOOPFILLBLACK 
+    D;JNE
+
+    @CHECKINPUT
+    0;JMP
+
+// Here just as precaution, program should never reach this part
+(END)
+    @END 
+    0;JMP
