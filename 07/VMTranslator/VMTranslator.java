2747b0193f5ad123f7e6754c601906b66f450d37
Tiago Ceccon <ceccon.t@hotmail.com>
2021-12-20 15:46:09 -0300

    Project 07 - VMTranslator accepts both files and directories as input

--- 07/VMTranslator/VMTranslator.java
+++ 07/VMTranslator/VMTranslator.java
@@ -1,18 +1,44 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 class VMTranslator {
 
+    private static final String VM_FILE_EXTENSION = ".vm";
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello!");
+        System.out.println("Starting translation...");
 
         if (args.length < 1) throw new Exception("ERROR! No source specified. Usage: '$ VMTranslator <source>'");
 
         String source = args[0];
 
-        File sourceFile = new File(source);
-        if (sourceFile.exists() && !sourceFile.isDirectory()) {
-            System.out.println("File exists");
+        List<String> toProcess = new ArrayList<>();
+
+        if (isVmFile(source)) {
+            toProcess.add(source);
+        } else {
+            File directory = new File(source);
+            if (!directory.isDirectory()) throw new Exception("ERROR! Source must either be a .vm file or a directory!");
+
+            File[] filesInFolder = directory.listFiles();
+            for (File file : filesInFolder) {
+                String name = file.getName();
+                if (isVmFile(name)) {
+                    toProcess.add(source + name);
+                }
+            }
         }
+
+        System.out.println("\nTranslating following files:");
+        for (String filename : toProcess) {
+            System.out.println(filename);
+        }
+
+    }
+
+    private static boolean isVmFile(String path) {
+        return path.endsWith(VM_FILE_EXTENSION);
     }
 
 }
