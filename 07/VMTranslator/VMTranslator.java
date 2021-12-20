c0b1970960df8229ee0809018134c24bd753b4eb
Tiago Ceccon <ceccon.t@hotmail.com>
2021-12-20 16:13:49 -0300

    Project 07 - VMTranslator determines target file name

--- 07/VMTranslator/VMTranslator.java
+++ 07/VMTranslator/VMTranslator.java
@@ -5,6 +5,7 @@ import java.util.List;
 class VMTranslator {
 
     private static final String VM_FILE_EXTENSION = ".vm";
+    private static final String TARGET_FILE_EXTENSION = ".asm";
 
     public static void main(String[] args) throws Exception {
         System.out.println("Starting translation...");
@@ -21,11 +22,13 @@ class VMTranslator {
             File directory = new File(source);
             if (!directory.isDirectory()) throw new Exception("ERROR! Source must either be a .vm file or a directory!");
 
+            String inputFolderPath = normalizeInputFolderPath(source);
+
             File[] filesInFolder = directory.listFiles();
             for (File file : filesInFolder) {
                 String name = file.getName();
                 if (isVmFile(name)) {
-                    toProcess.add(source + name);
+                    toProcess.add(inputFolderPath + name);
                 }
             }
         }
@@ -35,10 +38,45 @@ class VMTranslator {
             System.out.println(filename);
         }
 
+        String targetFile = generateTargetFilename(source);
+
+        System.out.println("\nTarget file for translation is " + targetFile);
+
     }
 
     private static boolean isVmFile(String path) {
         return path.endsWith(VM_FILE_EXTENSION);
+    }
+
+    private static String generateTargetFilename(String source) {
+        String target = "";
+
+        if (isVmFile(source)) {
+            int size = source.length();
+            int prefixSize = size - VM_FILE_EXTENSION.length();
+            target = source.substring(0, prefixSize) + TARGET_FILE_EXTENSION;
+        } else {
+            String inputFolderPath = normalizeInputFolderPath(source);
+            inputFolderPath = inputFolderPath.substring(0, inputFolderPath.length()-1); // facilitates getting dir name
+
+            int lastSeparator = inputFolderPath.lastIndexOf("/");
+            String dirName = inputFolderPath.substring(lastSeparator+1);
+            target = inputFolderPath + "/" + dirName + TARGET_FILE_EXTENSION;
+        }
+
+        return target;
+    }
+
+    private static String normalizeInputFolderPath(String folderPath) {
+        String path = "";
+
+        if (folderPath.endsWith("/")) {
+            path = folderPath + "";
+        } else {
+            path = folderPath + "/";
+        }
+
+        return path;
     }
 
 }
