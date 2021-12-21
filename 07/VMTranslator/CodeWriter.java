1a18ea7d2f27b830a876485146c5d6883df04a00
Tiago Ceccon <ceccon.t@hotmail.com>
2021-12-21 11:37:13 -0300

    Project 07 - Scaffold Parser and CodeWriter modules

---
+++ 07/VMTranslator/CodeWriter.java
@@ -0,0 +1,23 @@
+public class CodeWriter {
+
+    public CodeWriter(String outputFileName) {
+
+    }
+
+    public void setFileName(String fileName) {
+
+    }
+
+    public void writeArithmetic(String command) {
+
+    }
+
+    public void writePushPop(Command command, String segment, int index) {
+
+    }
+
+    public void close() {
+
+    }
+    
+}
---
+++ 07/VMTranslator/Parser.java
@@ -0,0 +1,27 @@
+public class Parser {
+
+    public Parser(String inputFileName) {
+
+    }
+
+    public boolean hasMoreCommands() {
+        return false;
+    }
+
+    public void advance() {
+
+    }
+
+    public Command commandType() {
+        return null;
+    }
+
+    public String arg1() {
+        return null;
+    }
+
+    public int arg2() {
+        return 0;
+    }
+    
+}
