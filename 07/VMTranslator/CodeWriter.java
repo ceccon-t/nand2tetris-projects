b757994de258bebdef9d942b8c760e6e79927c3b
Tiago Ceccon <ceccon.t@hotmail.com>
2021-12-21 11:59:04 -0300

    Project 07 - Initialize CodeWriter class implementation

--- 07/VMTranslator/CodeWriter.java
+++ 07/VMTranslator/CodeWriter.java
@@ -1,23 +1,88 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class CodeWriter {
 
-    public CodeWriter(String outputFileName) {
+    private FileWriter outputFileWriter;
+    private String currentInputFile;
 
+    public CodeWriter(String outputFileName) {
+        try {
+            File outputFile = new File(outputFileName);
+            if (outputFile.exists()) {
+                outputFile.delete();
+            }
+            outputFile.createNewFile();
+
+            outputFileWriter = new FileWriter(outputFileName);
+        } catch (IOException e) {
+            handleException(e);
+        }
     }
 
     public void setFileName(String fileName) {
+        currentInputFile = fileName;
 
+        try {
+            outputFileWriter.write("// " + currentInputFile);
+        } catch (IOException e) {
+            handleException(e);
+        }
     }
 
     public void writeArithmetic(String command) {
+        try {
+            outputFileWriter.write("// " + command);
 
+            // TODO: write hack code to output file
+        } catch (IOException e) {
+            handleException(e);
+        }
     }
 
     public void writePushPop(Command command, String segment, int index) {
+        try {
+            outputFileWriter.write("// " 
+                                    + commandToCommentRepresentation(command)
+                                    + " " + segment
+                                    + " " + index);
+
+            // TODO: write hack code to output file
+        } catch (IOException e) {
+            handleException(e);
+        }
 
     }
 
     public void close() {
+        try {
+            outputFileWriter.close();
+        } catch (IOException e) {
+            handleException(e);
+        }
+    }
 
+    private void handleException(Exception e) {
+        e.printStackTrace();
+        throw new RuntimeException(e.getMessage());
+    }
+
+    private String commandToCommentRepresentation(Command command) {
+        String representation = "";
+
+        switch (command) {
+            case C_PUSH:
+                representation = "push";
+                break;
+            case C_POP:
+                representation = "pop";
+                break;
+            default:
+                representation = "Unknown command";
+        }
+
+        return representation;
     }
     
 }
