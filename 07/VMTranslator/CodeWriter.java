9be61bbb3cbd591cffff6de390c9e1061963432d
Tiago Ceccon <ceccon.t@hotmail.com>
2021-12-31 16:53:54 -0300

    Project 07 - Implement arithmetic and logical operations

--- 07/VMTranslator/CodeWriter.java
+++ 07/VMTranslator/CodeWriter.java
@@ -6,6 +6,8 @@ public class CodeWriter {
 
     private FileWriter outputFileWriter;
     private String currentInputFile;
+    private Integer lineCount = 0;
+    private String labelPrefix;
 
     public CodeWriter(String outputFileName) {
         try {
@@ -16,6 +18,11 @@ public class CodeWriter {
             outputFile.createNewFile();
 
             outputFileWriter = new FileWriter(outputFileName);
+
+            labelPrefix = outputFileName
+                            .replace("/", "")
+                            .replace(".asm", "")
+                            .toUpperCase();
         } catch (IOException e) {
             handleException(e);
         }
@@ -32,33 +39,44 @@ public class CodeWriter {
     }
 
     public void writeArithmetic(String command) {
-        try {
-            outputFileWriter.write("// " + command + "\n");
+        writeLine("// " + command);
 
-            // TODO: write hack code to output file
-            if (command.equals("add")) {
-                writeAdd();
-            }
-        } catch (IOException e) {
-            handleException(e);
+        // TODO: write hack code to output file
+        // if (command.equals("add")
+        //     || command.equals("sub")
+        //     || command.equals("and")
+        //     || command.equals("or")) {
+        //     writeBinaryNumericalOperation(command);
+        // } 
+        switch(command) {
+            case "add":
+            case "sub":
+            case "and":
+            case "or":
+                writeBinaryNumericalOperation(command);
+                break;
+            case "neg":
+            case "not":
+                writeUnaryOperation(command);
+                break;
+            case "eq":
+            case "gt":
+            case "lt":
+                writeComparisonOperation(command);
+                break;
         }
     }
 
     public void writePushPop(Command command, String segment, int index) {
-        try {
-            outputFileWriter.write("// " 
-                                    + commandToCommentRepresentation(command)
-                                    + " " + segment
-                                    + " " + index
-                                    + "\n");
+        writeLine("// " 
+                    + commandToCommentRepresentation(command)
+                    + " " + segment
+                    + " " + index);
 
-            // TODO: write hack code to output file
-            if (command == Command.C_PUSH) {
-                writePush(segment, index);
-            } 
-        } catch (IOException e) {
-            handleException(e);
-        }
+        // TODO: write hack code to output file
+        if (command == Command.C_PUSH) {
+            writePush(segment, index);
+        } 
 
     }
 
@@ -73,56 +91,144 @@ public class CodeWriter {
     private void writePush(String segment, int index) {
 
         // Load value onto D register (assuming constant segment for now)
-        try {
-            outputFileWriter.write("@" + index + "\n");
-            outputFileWriter.write("D=A" + "\n");
-        } catch (IOException e) {
-            handleException(e);
-        }
+        writeLine("@" + index);
+        writeLine("D=A");
 
         // General push logic, applies to any segment
         // Assumes desired value is loaded onto D register
-        try {
-            // Go to next pos on stack
-            outputFileWriter.write("@SP" + "\n");
-            outputFileWriter.write("A=M" + "\n");
 
-            // Push value onto stack
-            outputFileWriter.write("M=D" + "\n");
+        // Go to next pos on stack
+        writeLine("@SP");
+        writeLine("A=M");
 
-            // Increment stack pointer
-            outputFileWriter.write("@SP" + "\n");
-            outputFileWriter.write("M=M+1" + "\n");
+        // Push value onto stack
+        writeLine("M=D");
 
-
-        } catch (IOException e) {
-            handleException(e);
-        }
+        // Increment stack pointer
+        writeLine("@SP");
+        writeLine("M=M+1");
 
     }
 
-    private void writeAdd() {
+    private void writeBinaryNumericalOperation(String operation) {
+        // Get value on top of stack
+        writeLine("@SP");
+        writeLine("A=M-1");
+        writeLine("D=M");
+
+        // Focus on next value on the stack
+        writeLine("A=A-1");
+
+        // Do the operation and save to the top of stack
+        switch(operation) {
+            case "add":
+                writeLine("M=M+D");
+                break;
+            case "sub":
+                writeLine("M=M-D");
+                break;
+            case "and":
+                writeLine("M=M&D");
+                break;
+            case "or":
+                writeLine("M=M|D");
+                break;
+        }
+
+        // Move stack pointer to next pos
+        writeLine("D=A+1");
+        writeLine("@SP");
+        writeLine("M=D");
+
+    }
+
+    private void writeUnaryOperation(String operation) {
+        // Go to top-most value of stack
+        writeLine("@SP");
+        writeLine("A=M-1");
+
+        // Aplly operation substituting value in place
+        switch(operation) {
+            case "neg":
+                writeLine("M=-M");
+                break;
+            case "not":
+                writeLine("M=!M");
+                break;
+        }
+    }
+
+    private void writeComparisonOperation(String operation) {
+        // Get value on top of stack
+        writeLine("@SP");
+        writeLine("A=M-1");
+        writeLine("D=M");
+
+        // Focus on next value on the stack
+        writeLine("A=A-1");
+
+        // Make comparison and store result on D
+        writeLine("D=M-D");
+
+        // Generate labels
+        String positiveLabel = generateLabel("POSITIVE");
+        String endLabel = generateLabel("END");
+
+        // Branch based on comparison
+        writeLine("@" + positiveLabel);
+        switch(operation) {
+            case "eq":
+                writeLine("D;JEQ");
+                break;
+            case "gt":
+                writeLine("D;JGT");
+                break;
+            case "lt":
+                writeLine("D;JLT");
+                break;
+        }
+
+        // Handle negative case
+        writeLine("D=0");   // To VM, False=0
+        // Go to the end of the branching
+        writeLine("@" + endLabel);
+        writeLine("0;JMP");
+
+        // Handle positive case
+        writeLine("(" + positiveLabel + ")"); // Labeling
+        writeLine("D=-1");  // To VM, True=-1
+
+        // End of brancing
+        writeLine("(" + endLabel + ")"); // Labeling
+        
+        // Write result to stack and update stack pointer
+
+        //   Go to correct position to place result
+        writeLine("@SP");
+        writeLine("A=M-1");
+        writeLine("A=A-1"); // Will remove the 2 topmost values
+
+        //   Write result to stack
+        writeLine("M=D");
+
+        //   Update stack pointer
+        writeLine("@SP");
+        writeLine("M=M-1");
+
+    }
+
+    private void writeLine(String line) {
         try {
-            // Get value on top of stack
-            outputFileWriter.write("@SP" + "\n");
-            outputFileWriter.write("A=M-1" + "\n");
-            outputFileWriter.write("D=M" + "\n");
-
-            // Focus on next value on the stack
-            outputFileWriter.write("A=A-1" + "\n");
-
-            // Do the Add and save to top of stack
-            outputFileWriter.write("M=M+D" + "\n");
-
-            // Move stack pointer to next pos
-            outputFileWriter.write("D=A+1" + "\n");
-            outputFileWriter.write("@SP" + "\n");
-            outputFileWriter.write("M=D" + "\n");
-
-
+            outputFileWriter.write(line + "\n");
+            lineCount++;
         } catch (IOException e) {
             handleException(e);
         }
+    }
+
+    private String generateLabel(String description) {
+        String newLabel = labelPrefix + "_" + lineCount + "_" + description;
+        return newLabel;
     }
 
     private void handleException(Exception e) {
